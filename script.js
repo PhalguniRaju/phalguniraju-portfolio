87c903fae79e80611bfeda3d50b46402934a8897
PhalguniRaju <phraju18@gmail.com>
2025-10-30 21:18:25 +0530

    Create script.js

---
+++ script.js
@@ -0,0 +1,15 @@
+// Scroll animation
+const elements = document.querySelectorAll('.animate');
+
+function checkScroll() {
+  const triggerBottom = window.innerHeight * 0.85;
+  elements.forEach(el => {
+    const boxTop = el.getBoundingClientRect().top;
+    if (boxTop < triggerBottom) {
+      el.classList.add('visible');
+    }
+  });
+}
+
+window.addEventListener('scroll', checkScroll);
+window.addEventListener('load', checkScroll);
